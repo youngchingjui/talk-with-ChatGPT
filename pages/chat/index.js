c5e10870d27ff1f7faa35ae02f58f9e0dfe256c7
youngchingjui <young.chingjui@gmail.com>
2023-01-28 10:24:00 +0800

    re-organizing data

--- pages/chat/index.js
+++ pages/chat/index.js
@@ -4,25 +4,7 @@ import { Container } from "react-bootstrap"
 import Message from "../../components/Message"
 import MicrophoneButton from "../../components/MicrophoneButton"
 import NavBar from "../../components/NavBar"
-
-const messages = [
-  {
-    text: "Explain to me how you work like I'm a 5-year-old.",
-    from: "user",
-  },
-  {
-    text: "I am a computer program that is designed to assist people with questions they have. I can understand what people ask me and provide answers based on my training and the information I have been given. I don't have a physical body like you do, so I can't see or touch things in the real world. I exist only in the computer and can only interact with people through a screen.",
-    from: "bot",
-  },
-  {
-    text: "Don't explain anything to me. just give me greek letter kappa",
-    from: "user",
-  },
-  {
-    text: "κ",
-    from: "bot",
-  },
-]
+import messages from "../../mocks/messages.json"
 
 const ChatPage = () => {
   const [recognizedSpeech, setRecognizedSpeech] = useState("")
