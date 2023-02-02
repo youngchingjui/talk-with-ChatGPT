1905162ce9991b0f1929d42caee552108e6d38c5
youngchingjui <young.chingjui@gmail.com>
2023-02-02 12:58:38 +0800

    refactored out original transcription API

--- components/MicrophoneButton.js
+++ components/MicrophoneButton.js
@@ -1,7 +1,8 @@
-import { useEffect, useRef, useState } from "react"
+import { useState } from "react"
 import ToastContainer from "react-bootstrap/ToastContainer"
 
-import micStatus from "../lib/models/micStatus"
+import useAAITranscriptionRecording from "../lib/hooks/useAAITranscriptionRecording"
+import useSpeechRecognitionRecording from "../lib/hooks/useSpeechRecognitionRecording"
 import MicIcon from "../public/svg/mic.svg"
 import UnsupportedBrowserToast from "./UnsupportedBrowserToast"
 
@@ -12,87 +13,20 @@ const MicrophoneButton = ({
   addMessage,
   language,
 }) => {
-  const [micState, setMicState] = useState(micStatus.idle)
   const [showUnsupportedBrowserToast, setShowUnsupportedBrowserToast] =
     useState(false)
-  const recognitionRef = useRef(null)
-  const resultRef = useRef("")
 
-  // Create new SpeechRecognition object that works across browsers, and assign it to recognitionRef
-  if (typeof window !== "undefined") {
-    window.SpeechRecognition =
-      window.SpeechRecognition || window.webkitSpeechRecognition
-    recognitionRef.current = new SpeechRecognition()
-    recognitionRef.current.lang = language
-    recognitionRef.current.interimResults = true
-  }
+  // const { isSocketReady, startRecording, stopRecording } =
+  //   useAAITranscriptionRecording({ setMessage: setResult })
 
-  // Update ref with latest result
-  useEffect(() => {
-    resultRef.current = result
-  }, [result])
-
-  const handlePointerDown = () => {
-    console.log("pointer down")
-    setMicState(micStatus.loading)
-
-    // Capture all events
-    // Main events
-    recognitionRef.current.addEventListener("start", () => {
-      console.log("started")
-      setMicState(micStatus.listening)
+  const { micState, startRecording, stopRecording } =
+    useSpeechRecognitionRecording({
+      result,
+      setResult,
+      language,
+      addMessage,
+      sendToOpenAI,
     })
-
-    recognitionRef.current.addEventListener("result", (event) => {
-      setResult(event.results[0][0].transcript)
-      console.log(event.results[0][0].transcript)
-    })
-
-    recognitionRef.current.addEventListener("end", (event) => {
-      console.log("ended")
-      addMessage({ text: resultRef.current, from: "user" })
-      sendToOpenAI(resultRef.current)
-      setResult("")
-      setMicState(micStatus.idle)
-    })
-
-    recognitionRef.current.addEventListener("error", (event) => {
-      console.log("Error:", event.error)
-      setShowUnsupportedBrowserToast(true)
-      setMicState(micStatus.idle)
-    })
-
-    // Other events
-    recognitionRef.current.addEventListener("audiostart", () => {
-      console.log("audiostart")
-    })
-    recognitionRef.current.addEventListener("audioend", () => {
-      console.log("audioend")
-    })
-    recognitionRef.current.addEventListener("soundstart", () => {
-      console.log("soundstart")
-    })
-    recognitionRef.current.addEventListener("soundend", () => {
-      console.log("soundend")
-    })
-    recognitionRef.current.addEventListener("speechstart", () => {
-      console.log("speechstart")
-    })
-    recognitionRef.current.addEventListener("speechend", () => {
-      console.log("speechend")
-    })
-    recognitionRef.current.addEventListener("nomatch", () => {
-      console.log("nomatch")
-    })
-
-    recognitionRef.current.start()
-  }
-
-  const handlePointerUp = () => {
-    console.log("pointer up")
-    recognitionRef.current.stop()
-    setMicState(micStatus.idle)
-  }
 
   // BUG: On Safari, speechrecognition will automatically send end event after speechend event, but audioend and soundend are not triggered. So microphone is still on, and transcription is still running, even after speech end.
   return (
@@ -107,8 +41,8 @@ const MicrophoneButton = ({
       >
         <button
           className={`microphone-button ${micState}`}
-          onPointerDown={handlePointerDown}
-          onPointerUp={handlePointerUp}
+          onPointerDown={startRecording}
+          onPointerUp={stopRecording}
         >
           <MicIcon height={70} width={44} />
         </button>
