77c9b38ef558548784f562f76667580011c153ba
youngchingjui <young.chingjui@gmail.com>
2023-02-02 22:17:48 +0800

    fixed playground bug for build

--- pages/playground/index.js
+++ pages/playground/index.js
@@ -1,21 +1,9 @@
 import Link from "next/link"
-import { Button } from "react-bootstrap"
-
-import useAAITranscriptionRecording from "../../lib/hooks/useAAITranscriptionRecording"
-import requestMicPermissions from "../../lib/utils/requestMicPermissions"
 
 const Playground = () => {
-  const { message, isSocketReady, startRecording, stopRecording } =
-    useAAITranscriptionRecording()
-
   return (
     <div>
       <Link href="/">Home</Link>
-      <Button onPointerDown={startRecording} onPointerOut={stopRecording}>
-        Hold to record
-      </Button>
-      <Button onClick={requestMicPermissions}>Request permission</Button>
-      <div>{message}</div>
     </div>
   )
 }
